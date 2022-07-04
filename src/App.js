9689c07bca53c996ca3f9c66ff5aba963931dd2e
Alvaro Arias <aarias@quodem.com>
2022-07-04 11:31:24 +0200

    fix sass

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter} from 'react-router-dom';
 import Header from './components/Header';
 import Main from './components/Main';
 
-import './App.css';
+import './styles/styles.scss';
 
 function App() {
   return (
