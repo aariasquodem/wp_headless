41b99edef422d9214a98f8407809d6c8e8d446cd
Alvaro Arias <aarias@quodem.com>
2022-07-04 09:22:18 +0200

    comments done

--- src/components/ArticleDetail/ArticleDetail.jsx
+++ src/components/ArticleDetail/ArticleDetail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useSearchParams } from "react-router-dom";
 import { useDataLoader } from 'react-use-data-loader';
 import {CircleLoader} from 'react-spinners';
@@ -8,10 +8,8 @@ import CommentCard from "../CommentCard";
 const article = async(id) => {
   try {
     const articleRes = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    const commentsRes = await axios.get(`https://jsonplaceholder.typicode.com/comments?postId=${id}`);
     const articleJson = articleRes.data
     const authorRes = await axios.get(`https://jsonplaceholder.typicode.com/users/${articleJson.userId}`);
-    const commentsJson = commentsRes.data;
     const authorJson = authorRes.data;
     const dataArticle = {
       'title': articleJson.title,
@@ -20,7 +18,6 @@ const article = async(id) => {
       'company': authorJson.company.name,
       'body': articleJson.body,
       'id': articleJson.id,
-      'comments': commentsJson
     }
     return dataArticle
   } catch (error) {
@@ -30,19 +27,42 @@ const article = async(id) => {
 
 const ArticleDetail = () => {
 
+  const id = window.location.href.split('=')[1];
+
   const [searchParams] = useSearchParams();
   const { data, loading } = useDataLoader(article, searchParams.get('id'));
+  const [comments, setComments] = useState([]);
 
-  const paintCards = () => data.comments.map((comment, i) => <CommentCard comment={comment} key={i}/>);
+  useEffect(() => {
+    const fetchComments = async() => {
+      try {
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/comments?postId=${id}`);
+        const json = res.data;
+        const commentsArr = json.map( element => {
+          return {
+            'email': element.email,
+            'body': element.body
+          }
+        })
+        setComments(commentsArr)
+      } catch (error) {
+        console.log('error', error)
+      }
+    }
+    fetchComments()
+  }, [])
+
+  console.log(comments);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const email = e.target.elements.email.value;
     const body = e.target.elements.body.value;
     const comment = {'email': email, 'body': body}
-    console.log(comment)
-    console.log(data.comments);
+    setComments([...comments, comment])
   }
+
+  const paintCards = () => comments.map((comment, i) => <CommentCard comment={comment} key={i}/>);
 
   return <>
           {loading
